a16891e81a8ca7822127445085b1981c9c61d8de
sophie nguyen <sophie.nguyen@berkeley.edu>
2025-11-05 17:36:12 -0800

    tested with carousel

--- src/app/true-wealth/page.tsx
+++ src/app/true-wealth/page.tsx
@@ -1,14 +1,22 @@
 import BlogBanner from "@/components/BlogBanner";
+import Carousel from "@/components/Carousel";
 
 export default function About(){
     return (
         <section className="space-y-6">
             <h1 className="text-xl font-bold">True Wealth</h1>
+            <Carousel itemsToShow={1}>
+            <BlogBanner
+                imageSrc="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+                articleTitle="Article Title"
+                articleSummary="Summary on the article"
+            />
             <BlogBanner
             imageSrc="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
-            articleTitle="Article Title"
-            articleSummary="Summary on the article"
+            articleTitle="Article Title 2"
+            articleSummary="Summary on the article 2"
             />
+            </Carousel>
         </section>
     )
   }
