3900c04fc3907d78f169a4135b2fdfd7c6588785
sophie nguyen <sophie.nguyen@berkeley.edu>
2025-12-03 12:24:22 -0800

    fixed button animations

--- src/app/pillars/page.tsx
+++ src/app/pillars/page.tsx
@@ -1,12 +1,20 @@
 export default function Pillars() {
     return (
-      <section className="space-y-6">
+      <section className="space-y-6 mx-30">
         <header className="space-y-2">
           <h1 className="text-3xl font-semibold">Pillars</h1>
           <p className="text-sm text-gray-500">
             A starter page — fill in content, components, and any calls-to-action here.
           </p>
         </header>
+
+        <a href="/investments">
+              <button 
+                className="items-center space-x-2 text-white font-medium px-8 py-3 rounded-xl
+                bg-(--teal) hover:bg-(--slate) active:scale-96 transition-all duration-200 active:duration-75">
+                <span>Investments Page</span>
+              </button>
+        </a>
   
         <div className="grid gap-6 md:grid-cols-2">
           <article className="rounded-2xl border p-4">
